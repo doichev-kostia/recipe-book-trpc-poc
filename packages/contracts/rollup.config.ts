daed8bf30aa3cb3e2c9998e8a8c898c69bd0b0bc
Kostiantyn Doichev <kostiantyn.doichev@panenco.com>
2023-01-21 22:16:42 +0100

    feat: extract prisma enums

--- packages/contracts/rollup.config.ts
+++ packages/contracts/rollup.config.ts
@@ -1,9 +1,12 @@
-import { RollupOptions } from "rollup";
+import { Plugin, RollupOptions } from "rollup";
+import esbuildPlugin from "rollup-plugin-esbuild";
+
+import { promisify } from "node:util";
 import * as path from "node:path";
 import { fileURLToPath } from "node:url";
-import esbuildPlugin from "rollup-plugin-esbuild";
-import nodeResolve from "@rollup/plugin-node-resolve";
-import commonJS from "@rollup/plugin-commonjs";
+import * as fs from "node:fs";
+
+import resolve from "resolve";
 
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
@@ -11,11 +14,56 @@ const src = path.resolve(__dirname, "src");
 const build = path.resolve(__dirname, "build");
 const entry = path.resolve(src, "index.ts");
 
-/**
- * Go to @trpc-poc/db package and transpile it
- * Get only enums from it
- * Put them in @trpc-poc/contracts package
- */
+const resolveImportPath = promisify(resolve);
+
+// extract prisma enums
+const enumsLoader: Plugin = {
+	name: "enums-loader",
+	async transform(code: string) {
+		const match = code.match(/export {.*} from "@trpc-poc\/database";/);
+		if (!match) return code;
+
+		// get values inside the { }
+		let enums = code.match(/(?<=\{ ).*(?=\s\})/);
+
+		if (!enums) return code;
+
+		enums = enums[0].split(", ");
+
+		const pkg: string = (await resolveImportPath("@trpc-poc/database"))
+			// truncate the path to the main package file
+			.split("/")
+			.slice(0, -1)
+			.join("/");
+
+		const content = await fs.promises.readFile(
+			`${pkg}/prisma/generated/prisma-client-es/index.js`,
+			"utf-8"
+		);
+
+		let enumsContent = "";
+
+		for (const enumName of enums) {
+			const regex = new RegExp(
+				`exports.${enumName} = makeEnum\\((.|\\n)*?\\);`,
+				"m"
+			);
+			const enumMatch = content.match(regex);
+			if (!enumMatch)
+				throw new Error(
+					`Enum ${enumName} not found in @trpc-poc/database package`
+				);
+
+			const enumContent = enumMatch[0]
+				.match(/(?<=\()(.|\n)*(?=\))/)?.[0]
+				.replaceAll(":", " =");
+
+			enumsContent += `export enum ${enumName} ${enumContent};\n\n`;
+		}
+
+		return enumsContent;
+	},
+};
 
 const config: RollupOptions = {
 	input: entry,
@@ -26,6 +74,7 @@ const config: RollupOptions = {
 		sourcemap: true,
 	},
 	plugins: [
+		enumsLoader,
 		esbuildPlugin({
 			include: /\.[jt]sx?$/,
 			sourceMap: true,
@@ -34,8 +83,6 @@ const config: RollupOptions = {
 			tsconfig: "tsconfig.json",
 			platform: "browser",
 		}),
-		nodeResolve({ extensions: [".js", ".ts"] }),
-		commonJS(),
 	],
 };
 
--- packages/contracts/src/enums/index.ts
+++ packages/contracts/src/enums/index.ts
@@ -1,3 +1,1 @@
-import { RoleType, TokenType } from "@trpc-poc/database";
-
-export { RoleType, TokenType };
+export { RoleType, TokenType } from "@trpc-poc/database";
