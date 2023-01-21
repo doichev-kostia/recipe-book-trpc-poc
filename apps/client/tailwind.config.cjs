b129e0c566dd34867d41dc640322fdbe5f5cfb71
Kostiantyn Doichev <kostiantyn.doichev@panenco.com>
2023-01-21 22:17:18 +0100

    feat: configs + dependencies

---
+++ apps/client/tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import("tailwindcss").Config} */
+module.exports = {
+	content: ["./index.html","./src/**/*.{js,jsx,ts,tsx,vue}"],
+	important: "#root",
+	theme: {
+		extend: {}
+	},
+	plugins: []
+};
